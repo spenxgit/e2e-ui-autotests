a8157167d31260fa1cb1d19a89d0bf4295be9890
BorisSpenx <bv@spenxy.com>
2023-10-05 14:39:50 +0300

    Gmail verification
    Gmail verification

--- tests/SignUp.ts
+++ tests/SignUp.ts
@@ -1,6 +1,11 @@
 import {TestConfigurator} from "../utils/TestConfigurator";
+import {User, Users} from "../data/UsersData";
 import TestUtils from "../utils/TestUtils";
+import MailUtils from "../utils/MailUtils";
 import SignUpPage from "../page_objects/SignUpPage";
+import MainPage from "../page_objects/MainDashboardPage";
+import LoginPage from "../page_objects/LoginPage";
+import MainDashboardPage from "../page_objects/MainDashboardPage";
 
 let loginPage;
 
@@ -12,17 +17,19 @@ fixture`Authentication`
     });
 
 test('Sign up as a new user', async t => {
+    //TODO дописать верификацию когда сайнап будет готов
+
     //generate new random email
-    let emailForNewUser = TestUtils.getRandomEmailAddress();
-    let passwordForNewUser = TestUtils.getRandomPassword();
-    console.log(emailForNewUser)
-    console.log(passwordForNewUser)
+    let newUser: User = new User(TestUtils.getRandomEmailAddress(), TestUtils.getRandomPassword());
+    await console.log(newUser.email)
+    await console.log(newUser.password)
 
     //sign up with a random email
     let signUpPage: SignUpPage = await loginPage.clickSignUpButton();
-    await signUpPage.fillSignUpData('Auto', 'Test', emailForNewUser, passwordForNewUser)
-
-    //TODO wait for feature, sign up is not working
+    await signUpPage.fillSignUpData('Auto', 'Test', newUser.email, newUser.password)
+    await MailUtils.validateSignUpWithMailLink(Users.UserA);
 
     //validation
+    await t.navigateTo(TestConfigurator.platformUrl);
+    let mainDashboardPage: MainDashboardPage = await loginPage.doLogin(newUser);
 });
---
+++ utils/MailUtils.ts
@@ -0,0 +1,29 @@
+import {t, Selector} from "testcafe";
+
+export default class MailUtils {
+    static async validateSignUpWithMailLink(User) {
+        await this.loginToGmail(User);
+        await this.openLastEmail();
+        await t.navigateTo(await this.getVerificationLink())
+
+    };
+
+    static async loginToGmail(User) {
+        await t.navigateTo('https://mail.google.com');
+        await t.typeText(Selector('input[type="email"]', {timeout: 20000}), User.email);
+        await t.click(Selector('#identifierNext > div > button > span'));
+        await t.typeText(Selector('input[type="password"]', {timeout: 20000}), User.password);
+        await t.click(Selector('#passwordNext > div > button > span'));
+    };
+
+    static async openLastEmail() {
+        await t.click(Selector('#\\:1q > span'));
+    }
+
+    static async getVerificationLink() {
+        const linkSelector = Selector('a').withText('Confirm email');
+        const verificationLink = linkSelector.getAttribute('href');
+        await t.setNativeDialogHandler(() => false);
+        return verificationLink;
+    };
+}
